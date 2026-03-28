4b5a92085149c1f2fa5b83e9d7eb2323487c7954
Pavan Bhosle <pavanbhosle1616@gmail.com>
2026-03-28 22:46:08 +0530

    Added shadcn UI components and Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button>🔥 Shadcn Working</Button>
+      <Button>login</Button>
+    </div>
+  )
+}
+
+export default App
